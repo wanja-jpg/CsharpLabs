7f5dd3dbbb8ea325256fa214b3ea2906ccc5376a
wanja-jpg <71464440+wanja-jpg@users.noreply.github.com>
2020-09-29 19:18:45 +0300

    Add files via upload

---
+++ csharp_lab1/csharp_lab1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lab1
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Швидкість=");
+			float speed = float.Parse(Console.ReadLine());
+			Console.WriteLine("Прискорення=");
+			float acceleration = float.Parse(Console.ReadLine());
+			Console.WriteLine("Відстань=");
+			float distance = float.Parse(Console.ReadLine());
+			float time = (distance + acceleration) / (2 * speed);
+			Console.WriteLine("Час=" + time);
+		}
+	}
+}
