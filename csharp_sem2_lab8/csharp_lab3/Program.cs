25d18d49f1a83aea2b879972f3b5528576598912
wanja-jpg <71464440+wanja-jpg@users.noreply.github.com>
2021-06-21 22:05:26 +0300

    Add files via upload

---
+++ csharp_sem2_lab8/csharp_lab3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int number;
+			do
+			{
+				Console.WriteLine("number=");
+				number = int.Parse(Console.ReadLine());
+				if (number < 100 || number > 999)
+				{
+					Console.WriteLine("Невірне значення");
+				}
+			} while (number < 100 || number > 999);
+
+			/*
+			int fnum = number % 10;
+			int snum = (number / 10) % 10;
+			int tnum = (number / 100) % 10;
+			*/
+			if (number % 10 < (number / 10) % 10 && (number / 10) % 10 < (number / 100) % 10)
+				Console.WriteLine("Послідовність зпадаюча");
+			else if (number % 10 > (number / 10) % 10 && (number / 10) % 10 > (number / 100) % 10)
+				Console.WriteLine("Послідовність зростаюча");
+			else
+				Console.WriteLine("Послідовність не зростаюча та не зпадаюча");
+		}
+    }
+}
