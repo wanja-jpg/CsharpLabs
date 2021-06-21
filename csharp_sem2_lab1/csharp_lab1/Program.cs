9b246e6e128f74f591526fa56aebdf26790bde4e
wanja-jpg <71464440+wanja-jpg@users.noreply.github.com>
2021-06-21 21:56:30 +0300

    Add files via upload

---
+++ csharp_sem2_lab1/csharp_lab1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lab1
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			static int Method(out int a) { a = 2; return a * 2; }
+			static void Main()
+			{
+				int operand = 3;
+				int result = Method(out operand);
+				Console.WriteLine("{0}; {1};", operand, result);
+
+			}
+		}
+	}
+}
